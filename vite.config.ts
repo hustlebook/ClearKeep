b35aa61ef1b90c4a5bc1af70b963f24be0b9d384
hustlebook <hustlebook55@gmail.com>
2025-07-17 14:03:42 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+// vite.config.js (for Vercel)
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ClearKeep/", // 👈 IMPORTANT: This must match your repo name
+  base: "./", // You can change to "/" if needed
   build: {
     outDir: "dist",
   },
